9a1a5030f93d967da8a35e60713694fcf26f771d
Yujiii0 <yuji918@naver.com>
2024-04-03 21:01:12 +0900

    ✨ feat: API index.ts 생성

---
+++ src/apis/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const client = axios.create({
+  baseURL: "https://port-0-express-server-17xco2nlsidlckv.sel5.cloudtype.app",
+  withCredentials: true,
+});
+
+const api = {};
+export default api;
