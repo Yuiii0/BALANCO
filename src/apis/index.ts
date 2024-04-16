56b77ffc582618d24197952e86efb2439ac765d9
Yujiii0 <yuji918@naver.com>
2024-04-16 18:07:30 +0900

    ♻️ refactor: auth.api 생성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
+import authAPI from "./auth/auth.api";
 
 export const client = axios.create({
   baseURL: "https://port-0-express-server-17xco2nlsidlckv.sel5.cloudtype.app",
   withCredentials: true,
 });
 
-const api = {};
+const api = {
+  auth: authAPI,
+};
 export default api;
