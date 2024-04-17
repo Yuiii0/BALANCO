60364abd9ba4b0395fe6c3bf5ba0084e1d984f1c
Yujiii0 <yuji918@naver.com>
2024-04-17 16:21:02 +0900

    ✨ feat: ErrorMessage 생성

--- src/components/Input/ErrorMessage/ErrorMessage.tsx
+++ src/components/Input/ErrorMessage/ErrorMessage.tsx
@@ -1,5 +1,19 @@
-function ErrorMessage({ children }: { children: React.ReactNode }) {
-  return <span className="font-light text-xs">{children}</span>;
+import React from "react";
+
+interface ErrorMessageProps {
+  children: React.ReactNode;
+}
+
+function ErrorMessage({ children }: ErrorMessageProps) {
+  return (
+    <div
+      className={`text-xs  px-2 mb-2 ${
+        children ? "text-red-500 opacity-100" : "opacity-0"
+      } h-2 `}
+    >
+      {children || ""}
+    </div>
+  );
 }
 
 export default ErrorMessage;
