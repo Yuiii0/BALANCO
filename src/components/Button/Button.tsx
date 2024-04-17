796671e6e20c8ebd34a95fad8a6336f629220ac0
Yujiii0 <yuji918@naver.com>
2024-04-17 16:21:58 +0900

    💄 design: Button rounded CSS 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,7 +11,7 @@ function Button({
 }: PropsWithChildren<ButtonProps>) {
   return (
     <button
-      className="border border-gray-800 py-4 px-12 text-[15px]  font-semibold w-full bg-white transition hover:bg-black hover:text-white rounded-sm active:bg-black active:text-white data-[color=black]:text-white data-[color=black]:bg-black  data-[color=black]:hover:bg-white data-[color=black]:hover:text-black data-[color=black]:active:text-black data-[color=black]:active:bg-white"
+      className="border border-gray-800 py-4 px-12 text-[15px]  font-semibold w-full bg-white transition hover:bg-black hover:text-white rounded-md active:bg-black active:text-white data-[color=black]:text-white data-[color=black]:bg-black  data-[color=black]:hover:bg-white data-[color=black]:hover:text-black data-[color=black]:active:text-black data-[color=black]:active:bg-white"
       data-color={color}
       {...props}
     >
