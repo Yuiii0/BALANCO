9ccdde4ecf7949236235c33bebc69d9cc96060cd
Yujiii0 <yuji918@naver.com>
2024-04-17 21:45:04 +0900

    ✨ feat: ProductCardsList 생성

---
+++ src/components/ProductCardsList/ProductCardsList.tsx
@@ -0,0 +1,23 @@
+import { Product } from "@/types/Product.type";
+import ProductCard from "../ProductCard/ProductCard";
+
+interface ProductCardsListProps {
+  products: Product[];
+}
+
+function ProductCardList({ products }: ProductCardsListProps) {
+  return (
+    <ul
+      className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-5
+    gap-y-10"
+    >
+      {products.map((product) => (
+        <li key={product.id}>
+          <ProductCard product={product} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ProductCardList;
