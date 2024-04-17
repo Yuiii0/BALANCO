4f78d0d0820f501b1f6adcc5fb945451194afc08
Yujiii0 <yuji918@naver.com>
2024-04-17 21:45:29 +0900

    ✨ feat: HomePage 구현

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -1,3 +1,17 @@
-export default function HomePage() {
-  return <main>HomePage</main>;
+import api from "@/apis";
+import Heading from "@/components/Heading";
+import Page from "@/components/Page";
+import ProductCardList from "@/components/ProductCardsList";
+
+async function HomePage() {
+  const products = await api.products.getProducts();
+
+  return (
+    <Page>
+      <Heading>New In</Heading>
+      <ProductCardList products={products} />
+    </Page>
+  );
 }
+
+export default HomePage;
