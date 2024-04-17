90d921e3fa3820f5cf462e95f3cf1082599272ed
Yujiii0 <yuji918@naver.com>
2024-04-17 16:19:01 +0900

    🐛 fix: forwardRef 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,24 +1,31 @@
-import { ComponentProps, useId } from "react";
+import { forwardRef, useId } from "react";
 
 interface InputProps extends ComponentProps<"input"> {
-  label: string;
+  label?: string;
+  error?: boolean;
 }
 
-function Input({ label, ...props }: InputProps) {
-  const id = useId();
-  return (
-    <div>
-      <label htmlFor={id} className="text-sm text-gray-800">
-        {label}
-      </label>
-      <input
-        type="text"
-        id={id}
-        className="border w-full px-6 py-3 focus:border-black outline-none transition rounded-sm border-gray-300"
-        {...props}
-      />
-    </div>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, ...props }, ref) => {
+    console.log(error);
+    const id = useId();
+    return (
+      <div>
+        <label htmlFor={id} className="text-sm text-gray-800">
+          {label}
+        </label>
+        <input
+          ref={ref}
+          type="text"
+          id={id}
+          className={`border w-full px-6 py-3 border-gray-300 outline-none transition rounded-md ${
+            error ? "focus:border-red-500" : "focus:border-black "
+          }`}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
