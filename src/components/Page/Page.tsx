48c765053ee89f9db9644bd04edefaac2fe2d756
Yujiii0 <yuji918@naver.com>
2024-04-18 15:04:36 +0900

    💄 design: padding값 수정

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -7,7 +7,7 @@ interface PageProps {
 function Page({ children, fullWidth }: PropsWithChildren<PageProps>) {
   return (
     <main
-      className="px-6 lg:px-8 py-6 lg:py-10 mx-auto max-w-screen-xl data-[full-width=true]:max-w-none flex flex-col grow w-full items-stretch"
+      className="px-6 lg:px-10 py-6 lg:py-10 mx-auto max-w-screen-xl data-[full-width=true]:max-w-none flex flex-col grow w-full items-stretch"
       data-full-width={fullWidth}
     >
       {children}
