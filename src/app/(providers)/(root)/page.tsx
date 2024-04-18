b023b6dd95059773d387c32f35c233d49b2a6c9a
Yujiii0 <yuji918@naver.com>
2024-04-18 23:03:13 +0900

    ✨ feat: search 쿼리 추가

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -12,10 +12,12 @@ interface PageChangeEvent {
   selected: number;
 }
 
-function HomePage() {
+function HomePage(props: { searchParams: { search?: string } }) {
   const [products, setProducts] = useState<Product[]>([]);
   const [pageNumber, setPageNumber] = useState(0);
   const itemsPerPage = 10;
+
+  const searchTerm = props.searchParams.search;
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -29,12 +31,20 @@ function HomePage() {
     setPageNumber(event.selected);
   };
 
+  // searchTerm에 따라 필터링된 상품 목록
+  const filteredProducts = searchTerm
+    ? products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : products;
+
+  //Pagination을 통해 display될 products 정의
   const pagesVisited = pageNumber * itemsPerPage;
-  const displayProducts = products.slice(
+  const displayProducts = filteredProducts.slice(
     pagesVisited,
     pagesVisited + itemsPerPage
   );
-  const pageCount = Math.ceil(products.length / itemsPerPage);
+  const pageCount = Math.ceil(filteredProducts.length / itemsPerPage);
 
   return (
     <Page>
