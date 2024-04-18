3012f89e7d71217cb5f05abc13326c1a7ff42559
Yujiii0 <yuji918@naver.com>
2024-04-18 23:04:07 +0900

    ✨ feat: 쿼리스트링 검색기능 추가

--- src/components/Header/components/SearchBar/SearchBar.tsx
+++ src/components/Header/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,35 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 function SearchBar() {
   const [isVisible, setIsVisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
 
-  const toggleInput = () => {
-    setIsVisible((prev) => !prev);
+  const handleClickSearchBtn = () => {
+    if (isVisible && searchTerm) {
+      router.push(`/?search=${encodeURIComponent(searchTerm)}`);
+      setSearchTerm("");
+    } else {
+      setIsVisible((prev) => !prev);
+    }
   };
 
   return (
-    <div className="flex gap-x-2 mr-14 relative  transition duration-500">
+    <div className="flex gap-x-2 relative pr-6  transition duration-500">
       {isVisible && (
         <input
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="샤넬"
+          value={searchTerm}
           className="border rounded-sm  focus:border-black transition-all duration-500 ease-in-out outline-none  cursor-pointer text-gray-500 font-light placeholder:text-gray-400 text-xs px-2 py-1"
         />
       )}
+
       <IoIosSearch
-        onClick={toggleInput}
+        onClick={handleClickSearchBtn}
         className="cursor-pointer w-full text-base mt-1 -translate-x-8"
       />
     </div>
