0c5afe1775ed7ca2f933ec62a3b6b043b1aac2d8
Yujiii0 <yuji918@naver.com>
2024-04-19 18:15:20 +0900

    ✨ feat: LikeProductButton 컴포넌트 생성

---
+++ src/components/LikeProductButton/LikeProductButton.tsx
@@ -0,0 +1,11 @@
+import { IoMdHeartEmpty } from "react-icons/io";
+
+function LikeProductButton() {
+  return (
+    <button>
+      <IoMdHeartEmpty />
+    </button>
+  );
+}
+
+export default LikeProductButton;
