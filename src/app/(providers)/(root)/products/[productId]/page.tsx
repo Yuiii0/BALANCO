d635cdcd39baa9bd7d308f6e427d0e958c4ea996
Yujiii0 <yuji918@naver.com>
2024-04-19 17:29:15 +0900

    🐛 fix: 상품 상세정보 수정

--- src/app/(providers)/(root)/products/[productId]/page.tsx
+++ src/app/(providers)/(root)/products/[productId]/page.tsx
@@ -2,14 +2,16 @@ import api from "@/apis";
 import Page from "@/components/Page";
 import ShoppingCart from "@/components/ShoppingCart";
 import { calculateDiscountPercentage } from "@/utils/calculateDiscount";
+import { calculateRewards } from "@/utils/calculateRewards";
 import formatPrice from "@/utils/formatPrice.utils";
+import { getDeliveryDate } from "@/utils/getDeliveryDate";
 import Image from "next/image";
 import Link from "next/link";
+import { IoIosHeartEmpty } from "react-icons/io";
 
 async function ProductPage(props: { params: { productId: string } }) {
   const productId = Number(props.params.productId);
   const product = await api.products.getProduct(productId);
-  console.log(product);
 
   return (
     <Page>
@@ -23,17 +25,17 @@ async function ProductPage(props: { params: { productId: string } }) {
             className="object-cover"
           />
         </div>
-        <div className="py-8 flex flex-col gap-y-2">
+        <div className="pb-8  pt-2 flex flex-col gap-y-2">
           <Link href={`/brands?brandId=${product.brandId}`}>
-            <h1 className="font-extrabold text-xl">{product.brand.nameEn}</h1>
+            <h1 className="font-extrabold ">{product.brand.nameEn}</h1>
           </Link>
-          <h1 className="text-[15px] border-b-2 border-black pb-5 text-slate-700">
+          <h1 className="text-xl font-light border-b-2 border-black pb-5 text-slate-700">
             {product.name}
           </h1>
           <div className="grid grid-cols-5 my-6 gap-x-5  gap-y-5 text-[15px] border-b-2 border-black pb-6">
             <div className="text-slate-900 font-bold">상품 금액</div>
             <div className="col-span-4 font-light line-through text-custom-gray">
-              {formatPrice(product.originalPrice)}
+              {`${formatPrice(product.originalPrice)}원`}
             </div>
             <div className="text-slate-900 font-bold ">판매가</div>
             <div className="col-span-4 font-extrabold text-lg -translate-y-0.5 font-notoSans">
@@ -45,11 +47,21 @@ async function ProductPage(props: { params: { productId: string } }) {
                 )}
               </span>
             </div>
+            <div className="text-slate-900 font-bold">적립금</div>
+            <div className="col-span-4 font-light">
+              {`${formatPrice(calculateRewards(product.price))}원`}
+            </div>
+            <div className="text-slate-900 font-bold">무이자할부</div>
+            <div className="col-span-4 font-light">최대 12개월</div>
             <div className="text-slate-900 font-bold">배송</div>
-            <div className="col-span-4 font-light">{product.deliveryType}</div>
+            <div className="col-span-4 font-semibold">{getDeliveryDate()}</div>
+            <div className="text-slate-900 font-bold">배송비</div>
+            <div className="col-span-4 font-light">무료</div>
             <div className="text-slate-900 font-bold ">잔여 재고</div>
             <div className="col-span-4 font-light">{`${product.onlineStock}개`}</div>
           </div>
+
+          <IoIosHeartEmpty />
           <ShoppingCart productId={productId} />
         </div>
       </section>
