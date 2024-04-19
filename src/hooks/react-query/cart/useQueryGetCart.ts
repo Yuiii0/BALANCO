aa63e1230fa4dfd5fe8c0d1fd0969637d2219af7
Yujiii0 <yuji918@naver.com>
2024-04-19 16:41:13 +0900

    🐛  fix: useQueryGetCart enabled 수정

--- src/hooks/react-query/cart/useQueryGetCart.ts
+++ src/hooks/react-query/cart/useQueryGetCart.ts
@@ -2,11 +2,11 @@ import api from "@/apis";
 import { useAuthStore } from "@/stores/auth/authStore";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useQueryGetCart() {
+export default function useQueryGetCart(enabled: boolean = false) {
   const { isLoggedIn } = useAuthStore();
   return useQuery({
     queryKey: ["cart"],
     queryFn: api.cart.getCart,
-    enabled: isLoggedIn,
+    enabled,
   });
 }
