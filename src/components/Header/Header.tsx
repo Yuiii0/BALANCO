c17ff2b57f74bb2756bd2f0dbae77eebfe888f2b
Yujiii0 <yuji918@naver.com>
2024-04-22 19:51:33 +0900

    ✨ feat: Logo Font 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -23,13 +23,13 @@ function Header() {
   return (
     <header className="sticky top-0 left-0 right-0 z-10 flex flex-col bg-white border-b ">
       <div className="flex justify-between items-center font-semibold border-t-[8px] border-black text-gray-900 px-14 pt-3 text-sm">
-        <p className="">력셔리 쇼핑은 Balanco</p>
+        <p>력셔리 쇼핑은 Balanco</p>
         <HeaderMenu />
       </div>
 
       <div className="flex justify-center py-3">
         <div className="text-5xl pb-4">
-          <Link href="/" className="font-bold ">
+          <Link href="/" className="font-bold font-oswald tracking-tighter">
             BALANCO
           </Link>
         </div>
@@ -38,7 +38,7 @@ function Header() {
         <SearchBar />
       </div>
       {showNav && (
-        <div className="flex pt-4">
+        <div className="flex">
           <nav className="mx-auto">
             <ul className="flex justify-center items-center gap-10 pb-3.5 max-w-screen-md font-semibold text-[15px]">
               <li className="hover:text-gray-600">
