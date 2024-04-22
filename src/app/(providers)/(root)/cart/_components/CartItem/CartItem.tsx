e48b34c02014b7b711358e77b3ee4c9a1b63a533
Yujiii0 <yuji918@naver.com>
2024-04-22 18:45:52 +0900

    💄 design: CartItem layout 수정

--- src/app/(providers)/(root)/cart/_components/CartItem/CartItem.tsx
+++ src/app/(providers)/(root)/cart/_components/CartItem/CartItem.tsx
@@ -13,35 +13,37 @@ function CartItem({ cartItem }: CartItemProps) {
   const product = cartItem.product;
 
   return (
-    <div className="flex justify-center">
-      <div className="flex h-40 sm:h-48 border-b py-5 gap-x-5 ">
-        <div className=" mr-2">
-          <Image
-            alt={product.name}
-            src={product.imgSrc}
-            width={115}
-            height={115}
-            className="object-cover"
-          />
-        </div>
+    <div>
+      <div className="flex justify-center">
+        <div className="flex h-40 sm:h-48 border-b py-5 gap-x-5 ">
+          <div className=" mr-2">
+            <Image
+              alt={product.name}
+              src={product.imgSrc}
+              width={115}
+              height={115}
+              className="object-cover"
+            />
+          </div>
 
-        <div className="flex flex-col w-[400px] pt-4">
-          <div className="font-bold">{product.brand.nameEn}</div>
-          <div className="font-light text-gray-600 text-[15px] mb-3">
-            {truncateText(product.name, 40)}
+          <div className="flex flex-col w-[400px] pt-4">
+            <div className="font-bold">{product.brand.nameEn}</div>
+            <div className="font-light text-gray-600 text-[15px] mb-3">
+              {truncateText(product.name, 40)}
+            </div>
+            <div className="text-[15px] font-semibold">{getDeliveryDate()}</div>
+            <div className="text-[13px] text-custom-gray">
+              {`잔여재고 ${product.onlineStock}개`}
+            </div>
           </div>
-          <div className="text-[15px] font-semibold">{getDeliveryDate()}</div>
-          <div className="text-[13px] text-custom-gray">
-            {`잔여재고 ${product.onlineStock}개`}
-          </div>
-        </div>
-        <ItemQuantityController
-          quantity={cartItem.quantity}
-          productId={product.id}
-        />
-        <div className="flex  w-28 justify-end self-center">
-          <div className="font-bold text-lg">
-            {`${formatPrice(product.price * cartItem.quantity)}원`}
+          <ItemQuantityController
+            quantity={cartItem.quantity}
+            productId={product.id}
+          />
+          <div className="flex  w-28 justify-end self-center">
+            <div className="font-bold text-lg">
+              {`${formatPrice(product.price * cartItem.quantity)}원`}
+            </div>
           </div>
         </div>
       </div>
