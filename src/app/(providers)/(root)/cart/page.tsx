35c5d98d097527fafdcf9dafbb907e461870cfe0
Yujiii0 <yuji918@naver.com>
2024-04-22 19:04:01 +0900

    🐛 fix: CartPage 조건부 렌더링 수정

--- src/app/(providers)/(root)/cart/page.tsx
+++ src/app/(providers)/(root)/cart/page.tsx
@@ -4,6 +4,7 @@ import Heading from "@/components/Heading";
 import Page from "@/components/Page";
 import useQueryGetCart from "@/hooks/react-query/cart/useQueryGetCart";
 import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 import CartItem from "./_components/CartItem";
 import TotalPrice from "./_components/TotalPrice";
 
@@ -16,33 +17,38 @@ function CartPage() {
     0
   );
 
-  if (!cart)
-    return (
-      <section>
-        <div>로그인 후 이용해주세요🙂 </div>
-      </section>
-    );
-
   return (
     <Page>
       <Heading>장바구니</Heading>
-
-      {cart?.items.length > 0 ? (
-        <section className="grid gap-x-16 gap-y-10 ">
-          <ul>
-            {cart?.items.map((cartItem) => (
-              <li key={cartItem.id}>
-                <CartItem cartItem={cartItem} />
-              </li>
-            ))}
-          </ul>
-          <TotalPrice totalPrice={totalPrice || 0} />
-        </section>
+      {cart ? (
+        cart.items.length > 0 ? (
+          <section>
+            <ul>
+              {cart?.items.map((cartItem) => (
+                <li key={cartItem.id}>
+                  <CartItem cartItem={cartItem} />
+                </li>
+              ))}
+            </ul>
+            <TotalPrice totalPrice={totalPrice || 0} />
+          </section>
+        ) : (
+          <section className="flex items-center justify-center flex-col gap-y-12">
+            <h6 className="text-lg font-semibold text-custom-gray">
+              장바구니가 비어 있습니다.
+            </h6>
+            <Link href={"/"}>
+              <div className="flex  gap-x-1 hover:underline">
+                <AiOutlineShopping className="text-lg" />
+                쇼핑하러 가기
+              </div>
+            </Link>
+          </section>
+        )
       ) : (
-        <section>
-          <h6>장바구니에 담긴 상품이 없습니다</h6>
-          <Link href={"/"}>쇼핑하러 가기</Link>
-        </section>
+        <h6 className="text-lg font-semibold mt-5 text-custom-gray text-center">
+          로그인 후 이용해주세요🙂{" "}
+        </h6>
       )}
     </Page>
   );
