288a6892e13d4c07e1aa7193e48c05c1724e6d68
Yujiii0 <yuji918@naver.com>
2024-04-24 22:46:46 +0900

    🐛 fix: 로그인시에만 fetch

--- src/hooks/react-query/cart/useQueryGetCart.ts
+++ src/hooks/react-query/cart/useQueryGetCart.ts
@@ -1,7 +1,7 @@
 import api from "@/apis";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useQueryGetCart(enabled: boolean = true) {
+export default function useQueryGetCart(enabled: boolean = false) {
   return useQuery({
     queryKey: ["cart"],
     queryFn: api.cart.getCart,
