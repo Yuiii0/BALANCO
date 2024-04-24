a04b2b67b50a8efe715e9d4bb0122d01f071d38b
Yujiii0 <yuji918@naver.com>
2024-04-24 14:31:57 +0900

    💄 design: LoadingSpinner 크기 수정

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,9 +1,9 @@
 function LoadingSpinner() {
   return (
-    <div role="status" className="flex justify-center items-center">
+    <div role="status" className="flex justify-center items-center pt-24">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-black"
+        className="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-black"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
