d6953a9a96ecf72e5419c0d59145934c602331eb
Yujiii0 <yuji918@naver.com>
2024-04-25 01:05:48 +0900

    💄 design: SearchBar 모바일 위치 수정

--- src/components/Header/components/SearchBar/SearchBar.tsx
+++ src/components/Header/components/SearchBar/SearchBar.tsx
@@ -26,27 +26,26 @@ function SearchBar() {
 
   const handleSearch = () => {
     const term = searchTerm || placeholder;
-    console.log("term", term);
     router.push(`/?search=${encodeURIComponent(term)}`);
     setSearchTerm("");
   };
 
   return (
     <>
-      <div className="flex gap-x-2 relative pr-6 transition duration-500">
+      <div className="flex gap-x-2 relative pr-6 sm-max:pr-5 transition duration-500 sm-max:-translate-y-4 ">
         <input
           onInput={(e) => setSearchTerm(e.currentTarget.value)}
           onKeyDown={handleKeyDown}
           placeholder={placeholder}
           value={searchTerm}
-          className={`border rounded-sm focus:border-black transition-all duration-500 ease-in-out outline-none cursor-pointer text-gray-500 font-light placeholder:text-gray-400 text-xs px-2 py-1 ${
-            isVisible ? "w-40 opacity-100" : "w-0 opacity-0"
+          className={`border rounded-sm focus:border-black transition-all duration-500 ease-in-out outline-none cursor-pointer text-gray-500 font-light placeholder:text-gray-400 text-xs px-2 py-1 sm-max:py-0 ${
+            isVisible ? "w-40 sm-max:w-20 opacity-100" : "w-0 opacity-0"
           }`}
         />
       </div>
       <IoIosSearch
         onClick={handleClickSearchBtn}
-        className="cursor-pointer w-full text-lg mt-1 -translate-x-12"
+        className="cursor-pointer w-full text-lg mt-1 -translate-x-12 sm-max:-translate-x-4 sm-max:-translate-y-5 "
       />
     </>
   );
