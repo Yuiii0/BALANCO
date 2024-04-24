7fe2c7fef0f7bd90075f29a9df4cf53d1d197924
Yujiii0 <yuji918@naver.com>
2024-04-24 14:49:59 +0900

    🐛 fix: ErrorMessage 동적 변경

--- src/components/ErrorComponent/ErrorComponent.tsx
+++ src/components/ErrorComponent/ErrorComponent.tsx
@@ -1,10 +1,14 @@
 import { MdOutlineErrorOutline } from "react-icons/md";
 
-function ErrorComponent() {
+interface ErrorComponentProps {
+  message?: string;
+}
+
+function ErrorComponent({ message = "Error" }: ErrorComponentProps) {
   return (
     <div className="flex justify-center items-center flex-col pt-20">
-      <MdOutlineErrorOutline className="text-6xl" />
-      <span className="font-bold text-lg">Error</span>
+      <MdOutlineErrorOutline className="text-6xl mb-1" />
+      <span className="font-bold text-lg">{message}</span>
     </div>
   );
 }
