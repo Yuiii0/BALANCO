613689396e06f9139ef67c9e48449be8e0503587
Yujiii0 <yuji918@naver.com>
2024-04-25 16:43:28 +0900

    ✨ feat: CSProfile 생성

---
+++ src/components/CSChat/components/ChatSection/CSProfile/CSProfile.tsx
@@ -0,0 +1,11 @@
+import { RiCustomerService2Line } from "react-icons/ri";
+
+function CSProfile() {
+  return (
+    <div className="bg-bg-pink min-w-12 h-12 flex justify-center items-center rounded-2xl">
+      <RiCustomerService2Line className="text-[28px]" />
+    </div>
+  );
+}
+
+export default CSProfile;
