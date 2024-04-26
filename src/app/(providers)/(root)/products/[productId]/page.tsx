b05717def2dbc03bac950b3cfa1dabae2aa053d0
Yujiii0 <yuji918@naver.com>
2024-04-26 18:35:49 +0900

    🐛 fix: 모바일 상세페이지 이미지 문제 해결

--- src/app/(providers)/(root)/products/[productId]/page.tsx
+++ src/app/(providers)/(root)/products/[productId]/page.tsx
@@ -17,7 +17,7 @@ async function ProductPage(props: { params: { productId: string } }) {
   return (
     <Page>
       <section className="grid grid-cols-1 sm:grid-cols-2 gap-x-16 pt-2 ">
-        <div className="relative ">
+        <div className="relative sm-max:w-full h-[420px]  bg-red-100">
           <Image
             alt={product.name}
             src={product.imgSrc}
