864f3b3f6cbd463c79e70341784b23e66b03cdb1
Yujiii0 <yuji918@naver.com>
2024-04-26 13:48:22 +0900

    ✨ feat: server에 sendMessage

--- src/components/CSChat/components/ChatSection/ChatRoom/ChatRoom.tsx
+++ src/components/CSChat/components/ChatSection/ChatRoom/ChatRoom.tsx
@@ -1,31 +1,61 @@
 "use client";
 
-import FileInput from "@/components/FileInput";
-import EmojiPicker from "emoji-picker-react";
-import { useState } from "react";
-import { FaRegSmile } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { FaAngleLeft } from "react-icons/fa6";
+import socket from "../../../../../utils/socket/socket.js";
 import CustomerProfile from "../CSProfile";
-import ChatMessage from "./ChatMessage";
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList/MessageList.jsx";
 
-interface ChattingRoomProps {
+interface ChatRoomProps {
   setShowChatRoom: (value: boolean) => void;
 }
 
-function ChatRoom({ setShowChatRoom }: ChattingRoomProps) {
-  const [showPicker, setShowPicker] = useState(false);
+function ChatRoom({ setShowChatRoom }: ChatRoomProps) {
+  const [user, setUser] = useState(null);
   const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
   const handleClickGoBack = () => {
     setShowChatRoom(false);
   };
 
-  const onEmojiClick = (event: any, emojiObject: any) => {
-    setMessage((prevMessage) => prevMessage + event.emoji);
-    setShowPicker(false);
-  };
+  useEffect(() => {
+    //socket이 메시지를 듣는다
+    socket.on("message", (message) => {
+      console.log("res", message);
+      setMessageList((prev) => prev.concat(message));
+    });
+    askUserName();
 
-  console.log("showPicker", showPicker);
-  console.log("message", message);
+    return () => {
+      socket.off("message");
+    };
+  }, []);
+
+  const askUserName = () => {
+    if (!user) {
+      const userName = prompt("이름 입력");
+      console.log("userName", userName);
+      //서버에게 말하는 함수 emit(대화 제목,보낼내용,콜백함수)
+      socket.emit("login", userName, (res: any) => {
+        //서버에서 ok가 오면,(닉네임이 잘오면)
+        if (res?.ok) {
+          setUser(res.data);
+        }
+      });
+    }
+  };
+  const sendMessage = (event: any) => {
+    event.preventDefault();
+    socket.emit("sendMessage", message, (res: any) => {
+      console.log("sendMessageRds", res);
+    });
+    setMessage("");
+  };
+  // console.log("user", user);
+  // console.log("message", message);
+  // console.log("messageList", messageList);
   return (
     <section className="w-[390px] h-[610px]  bg-white shadow-xl fixed bottom-28 rounded-2xl   right-8 ">
       <div className="flex gap-x-8 px-4 py-4 items-center">
@@ -44,25 +74,13 @@ function ChatRoom({ setShowChatRoom }: ChattingRoomProps) {
         </div>
       </div>
       <div className="px-8 py-10">
-        <ChatMessage message="무엇을 도와드릴까요? 🙂🙏🏻" sender="Balanco" />
-        <div className="absolute bottom-4 w-11/12 left-4 ">
-          <input
-            className=" bg-bg-lightgray w-full px-4 py-3 outline-none rounded-xl  hover:bg-hover-lightgray transition text-gray-400"
-            placeholder="메시지를 입력하세요."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></input>
-          <div className="absolute top-4 right-4 flex items-center gap-x-3">
-            <FaRegSmile
-              onClick={() => setShowPicker((prev) => !prev)}
-              className=" text-custom-gray cursor-pointer text-lg hover:text-gray-600 "
-            />
-            <FileInput />
-          </div>
-          {showPicker && (
-            <EmojiPicker onEmojiClick={onEmojiClick}></EmojiPicker>
-          )}
-        </div>
+        {/* <ChatMessage message="무엇을 도와드릴까요? 🙂🙏🏻" sender="Balanco" /> */}
+        <MessageList messageList={messageList} user={user} />
+        <MessageInputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
     </section>
   );
