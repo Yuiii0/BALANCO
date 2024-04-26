010d15a86c4a6c410679f4ccd9feb32cd4f66977
Yujiii0 <yuji918@naver.com>
2024-04-26 15:15:53 +0900

    💄 design: scrollBar, css 추가

--- src/components/CSChat/components/ChatSection/ChatRoom/MessageList/MessageList.jsx
+++ src/components/CSChat/components/ChatSection/ChatRoom/MessageList/MessageList.jsx
@@ -1,19 +1,19 @@
 function MessageList({ messageList, user }) {
   console.log("messageList,,,,", messageList);
   return (
-    <div>
-      {messageList.map((message, index) => {
+    <div className="h-[425px] overflow-scroll scrollbar-hide">
+      {messageList.map((message) => {
         return (
-          <div key={message._id} className="text-sm">
+          <div key={message._id} className="text-sm flex ">
             {message.user.name === "system" ? (
-              <div className="flex flex-col">
+              <div className="flex flex-col justify-start">
                 <span className="text-custom-gray fs-xs px-2">Balanco</span>
-                <p className="bg-hover-lightgray px-3 mb-3 text-wrap py-2 rounded-lg max-w-[200px] ">
+                <p className="bg-hover-lightgray px-3 mb-3 py-2 rounded-lg max-w-[200px] ">
                   {message.chat}
                 </p>
               </div>
             ) : (
-              <div className="flex justify-end">
+              <div className="ml-auto">
                 <p className="bg-bg-pink px-3 py-2 text-wrap mb-3 rounded-lg max-w-[200px]">
                   {message.chat}
                 </p>
