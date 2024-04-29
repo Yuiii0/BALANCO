bdbc3d2fa3626b861efe0723393cdd8de086ce1b
Yujiii0 <yuji918@naver.com>
2024-04-29 15:21:36 +0900

    🐛 fix: cartItem 수량 조절시 Link이동 문제 해결

--- src/app/(providers)/(root)/cart/_components/CartProduct/CartProduct.tsx
+++ src/app/(providers)/(root)/cart/_components/CartProduct/CartProduct.tsx
@@ -14,34 +14,36 @@ function CartProduct({ cartItem }: CartProductProps) {
   const product = cartItem.product;
 
   return (
-    <Link
-      href={`/products/${product.id}`}
-      className="sm-max:flex sm-max:relative sm:pb-2 sm-max:justify-center "
-    >
-      <div className="flex justify-center sm-max:block  ">
-        <div className="flex h-48 sm-max:h-[220px] border-b py-5 gap-x-5 sm-max:gap-x-3 sm-max:max-w-[340px]  ">
-          <div className="mr-2 sm-max:pt-2 ">
-            <Image
-              alt={product.name}
-              src={product.imgSrc}
-              width={115}
-              height={115}
-              className="object-cover sm-max:w-full"
-            />
-          </div>
-          <div className="flex flex-col w-[400px] sm:max-w-[300px] sm-max:pt-7 pt-4">
-            <div className="font-bold">{product.brand.nameEn}</div>
-            <div className="font-light text-gray-600 text-[15px] mb-2 sm-max:mb-3">
-              {truncateText(product.name, 40)}
+    <div className="sm-max:flex sm-max:relative sm:pb-2 sm-max:justify-center ">
+      <div className="flex justify-center sm-max:block ">
+        <div className="flex h-48 sm-max:h-[200px] border-b py-5 gap-x-5 sm-max:gap-x-3 sm-max:max-w-[340px]">
+          <Link
+            href={`/products/${product.id}`}
+            className="flex sm-max:h-[220px] sm-max:gap-x-3 sm-max:max-w-[340px] gap-x-3 "
+          >
+            <div className="mr-2 sm-max:pt-2 ">
+              <Image
+                alt={product.name}
+                src={product.imgSrc}
+                width={115}
+                height={115}
+                className="object-cover sm-max:w-full"
+              />
             </div>
-            <div className="text-[15px] font-semibold sm-max:text-sm">
-              {getDeliveryDate()}
+            <div className="flex flex-col w-[400px] sm:max-w-[300px] pt-4">
+              <div className="font-bold">{product.brand.nameEn}</div>
+              <div className="font-light text-gray-600 text-[15px] mb-2 sm-max:mb-3 sm-max:overflow-hidden sm-max:h-4 ">
+                {truncateText(product.name, 26)}
+              </div>
+              <div className="text-[15px] font-semibold sm-max:text-sm">
+                {getDeliveryDate()}
+              </div>
+              <div className="text-[13px] text-custom-gray">
+                {`잔여재고 ${product.onlineStock}개`}
+              </div>
             </div>
-            <div className="text-[13px] text-custom-gray">
-              {`잔여재고 ${product.onlineStock}개`}
-            </div>
-          </div>
-          <div className="flex gap-x-3 sm-max:justify-between sm-max:w-full sm-max:pt-4 sm-max:absolute sm-max:top-[168px] sm-max:px-3 ">
+          </Link>
+          <div className="flex gap-x-3 sm-max:justify-between sm-max:w-full  sm-max:absolute sm-max:top-[150px] sm-max:px-3 ">
             <ItemQuantityController
               quantity={cartItem.quantity}
               productId={product.id}
@@ -54,7 +56,7 @@ function CartProduct({ cartItem }: CartProductProps) {
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
