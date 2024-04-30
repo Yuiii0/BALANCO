cc5ecd59150b44d86c1e161a3caebf4f48db790b
Yujiii0 <yuji918@naver.com>
2024-04-30 20:31:26 +0900

    🐛 fix: Heading 태그 수정

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,8 +1,8 @@
 function Heading({ children }: { children: React.ReactNode }) {
   return (
-    <h2 className="font-bold text-3xl sm-max:text-2xl text-center mb-12 sm-max:mb-6">
+    <h1 className="font-bold text-3xl sm-max:text-2xl text-center mb-12 sm-max:mb-6">
       {children}
-    </h2>
+    </h1>
   );
 }
 
