81207b80d1e4bc40e048e23f0295c7f9762f4276
Yujiii0 <yuji918@naver.com>
2024-04-30 20:32:01 +0900

    ✨ feat: openGraph 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,15 @@ export default function HTMLLayout({
 }>) {
   return (
     <html lang="ko">
+      <Head>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content="명품 쇼핑은 Balanco"></meta>
+        <meta
+          property="og:description"
+          content="력셔리 쇼핑몰 Balanco입니다"
+        ></meta>
+        <meta property="og:locale" content="ko_KR"></meta>
+      </Head>
       <body>{children}</body>
     </html>
   );
